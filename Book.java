94f8e7dca4ee84fef011c00ea4cafaa1003f5509
charliembrodie <cbrodi01@cspc061.dcs.bbk.ac.uk>
2012-11-14 16:55:38 +0000

    I am up to around 2.6 and have just added addBook functionality but no test for it as of yet.

---
+++ Book.java
@@ -0,0 +1,11 @@
+public interface Book{
+    /**
+	* @return title of the book
+	*/
+	public String getTitle(); //Gets the title of the book.
+	
+	/**
+	* @return the author of the book
+	*/
+	public String getAuthor(); //Gets the author of the book.
+}
